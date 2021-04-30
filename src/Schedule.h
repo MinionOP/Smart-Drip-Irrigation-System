1f467fd19ffed7f8c5e1f4712a6ee2e850d52239
MinionOP <73415249+MinionOP@users.noreply.github.com>
2021-04-30 17:23:19 -0500

    Create Schedule.h

---
+++ src/Schedule.h
@@ -0,0 +1,21 @@
+#ifndef SCHEDULE_H
+#define SCHEDULE_H
+
+
+enum day{MONDAY = 0, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY, NUM_DAY};
+
+
+
+class Schedule{
+public:
+    
+
+
+
+
+private:
+
+};
+
+
+#endif
